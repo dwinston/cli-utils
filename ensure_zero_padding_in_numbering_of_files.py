0ed693edd6e9a625f09f7e3c9f83df0042622d0a
denarced <denarced@gmail.com>
2017-04-13 11:24:20 +0300

    Tolerate empty directories. Before this change executing max function would crash because it was provided with an empty iterable. Now max is only called if there's at least one item and we know that there is if iteration of the iterable numbered_fnames has begun.

--- ensure_zero_padding_in_numbering_of_files.py
+++ ensure_zero_padding_in_numbering_of_files.py
@@ -22,11 +22,12 @@ def main():
     numbered_fnames = [fname for fname in os.listdir(path)
                        if numbered.search(fname)]
 
-    max_digits = max(len(numbered.search(fname).group(2))
-                     for fname in numbered_fnames)
-
+    max_digits = None
     for fname in numbered_fnames:
         _, prefix, num, ext, _ = numbered.split(fname, maxsplit=1)
+        if max_digits is None:
+            max_digits = max(len(numbered.search(fname).group(2))
+                             for fname in numbered_fnames)
         num = num.zfill(max_digits)
         new_fname = "{}{}.{}".format(prefix, num, ext)
         if fname != new_fname:
